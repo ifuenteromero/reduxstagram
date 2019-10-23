9f798425d881640c5815fc9a59264c3e33328a55
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2019-10-23 18:18:36 +0200

    add copy styles

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>
+              <Link to="/">Reduxstagram</Link>
+            </h1>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import './styles/index.scss';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+ 
+ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>,
+document.getElementById('root')
+);
